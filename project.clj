1121e1eb1f7de161728f062a3a5ba6af3e1bc346
Nick <nv4@rice.edu>
2012-05-30 23:18:10 -0500

    Added Conrad Barski's Vijual library to use instead of Graphviz (dot) for Grand Theft Wumpus.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject land-of-lisp "0.0.1"
   :description "Land of Lisp games in Clojure"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/math.numeric-tower "0.0.1"]])
